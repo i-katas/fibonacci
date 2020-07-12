adf85c46a641a6613085187c6f33f09a2e683b23
i-katas <feexonj@gmail.com>
2020-07-12 23:10:47 +0800

    Raised exception if the result of fib(n) is overflowed

--- src/main/java/com/katas/Fib.java
+++ src/main/java/com/katas/Fib.java
@@ -1,14 +1,17 @@
 package com.katas;
 
 public class Fib {
-  public static int fib(int i) {
-    if (i < 1) {
-        throw new IllegalArgumentException();
+    public static int fib(final int i) {
+        if (i < 1) {
+            throw new IllegalArgumentException();
+        }
+        int sum = 0;
+        for(int prev = 1, n = i; n > 0; n--) {
+            sum = prev + (prev = sum);
+            if(prev > sum) {
+                throw new IllegalArgumentException("fib(" + i + ") is overflowed");
+            }
+        }
+        return sum;
     }
-    int sum = 0;
-    for(int prev = 1; i > 0; i--) {
-      sum = prev + (prev = sum);
-    }
-    return sum;
-  }
 }
--- src/test/java/com/katas/FibTest.java
+++ src/test/java/com/katas/FibTest.java
@@ -3,26 +3,34 @@ package com.katas;
 import org.junit.Test;
 import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertThat;
+import static org.junit.internal.matchers.ThrowableMessageMatcher.hasMessage;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.containsString;
 
 import static com.katas.Fib.fib;
 
 public class FibTest {
-  @Test
-  public void return1ForTheFirstTwoIndecies() throws Throwable {
-    assertThat(fib(1), equalTo(1));
-    assertThat(fib(2), equalTo(1));
-  }
+    @Test
+    public void return1ForTheFirstTwoIndecies() throws Throwable {
+        assertThat(fib(1), equalTo(1));
+        assertThat(fib(2), equalTo(1));
+    }
 
-  @Test
-  public void sumOfPrevious2IndicesResultIfIndexLargeThan2() throws Throwable {
-    assertThat(fib(3), equalTo(2));
-    assertThat(fib(4), equalTo(3));
-    assertThat(fib(5), equalTo(5));
-  }
+    @Test
+    public void sumOfPrevious2IndicesResultIfIndexLargeThan2() throws Throwable {
+        assertThat(fib(3), equalTo(2));
+        assertThat(fib(4), equalTo(3));
+        assertThat(fib(5), equalTo(5));
+    }
 
-  @Test
-  public void throwsExceptionIfIndexLessThan1() throws Throwable {
-    assertThrows(IllegalArgumentException.class, () -> fib(0));
-  }
+    @Test
+    public void throwsExceptionIfIndexLessThan1() throws Throwable {
+        assertThrows(IllegalArgumentException.class, () -> fib(0));
+    }
+
+    @Test
+    public void throwsExceptionIfResultOverflowed() throws Throwable {
+        Exception expected = assertThrows(IllegalArgumentException.class, () -> fib(47));
+        assertThat(expected, hasMessage(containsString("fib(47) is overflowed")));
+    }
 }
