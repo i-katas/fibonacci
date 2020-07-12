707a83e1e9b4a47b977e594c9b4e898867efb291
i-katas <feexonj@gmail.com>
2020-07-12 23:01:04 +0800

    Removed temporary variable `tmp`

--- src/main/java/com/katas/Fib.java
+++ src/main/java/com/katas/Fib.java
@@ -3,10 +3,8 @@ package com.katas;
 public class Fib {
   public static int fib(int i) {
     int sum = 0;
-    for(int tmp, prev = 1; i > 0; i--) {
-      tmp = sum;
-      sum += prev;
-      prev = tmp;
+    for(int prev = 1; i > 0; i--) {
+      sum = prev + (prev = sum);
     }
     return sum;
   }
