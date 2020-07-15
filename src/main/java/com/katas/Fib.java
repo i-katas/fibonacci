2d48cf0130b87c81417cff8f5070569f1da03c08
i-katas <feexonj@gmail.com>
2020-07-15 23:23:34 +0800

    Reset starting value for the loop

--- src/main/java/com/katas/Fib.java
+++ src/main/java/com/katas/Fib.java
@@ -5,8 +5,8 @@ public class Fib {
         if (i < 1) {
             throw new IllegalArgumentException();
         }
-        int result = 0;
-        for(int prev = 1, n = i; n > 0; n--) {
+        int result = 1;
+        for(int prev = 1, n = i; n > 2; n--) {
             result = prev + (prev = result);
             if(prev > result) {
                 throw new IllegalArgumentException("fib(" + i + ") is overflowed");
